2afafc6688767053a431d002b901b48b2b9ff313
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-10-11 16:58:09 +0200

    Vercel stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
+    output: "static",
     site: "https://personaplus.vercel.app/",
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true,
+        },
+    }),
     compressHTML: true,
 });
