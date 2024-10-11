c91f9bf67b908048a9e70cc340615064d9eb8e9e
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-10-11 17:02:01 +0200

    Like this?

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,9 @@ export default defineConfig({
         webAnalytics: {
             enabled: true,
         },
+        speedInsights: {
+            enabled: true,
+        },
     }),
     compressHTML: true,
 });
