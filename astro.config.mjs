e2a4b258378db989b720b5df303240c7f8b8e8b0
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-12-05 18:36:10 +0100

    Fix deprecated import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,23 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import vercelStatic from "@astrojs/vercel/static";
+import vercelStatic from "@astrojs/vercel";
 import mdx from "@astrojs/mdx";
 // import SpeedInsights from "@vercel/speed-insights/";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  site: "https://personaplus.vercel.app/",
+    output: "static",
+    site: "https://personaplus.vercel.app/",
 
-  adapter: vercelStatic({
-      webAnalytics: {
-          enabled: true,
-      },
-      speedInsights: {
-          enabled: true,
-      },
-  }),
+    adapter: vercelStatic({
+        webAnalytics: {
+            enabled: true,
+        },
+        speedInsights: {
+            enabled: true,
+        },
+    }),
 
-  compressHTML: true,
-  integrations: [mdx()],
+    compressHTML: true,
+    integrations: [mdx()],
 });
